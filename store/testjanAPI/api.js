2b8485f12f4c03825dd240c434f22f0159490870
Crowdbotics <engineering@crowdbotics.com>
2023-09-06 13:42:43 +0000

    Update store.

--- store/testjanAPI/api.js
+++ store/testjanAPI/api.js
@@ -1,72 +1,50 @@
-import axios from "axios";
+import axios from "axios"
 const testjanAPI = axios.create({
   baseURL: "https://test-jan-080523-1.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return testjanAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return testjanAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return testjanAPI.post(`/api/v1/login/`, payload);
+  return testjanAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return testjanAPI.post(`/api/v1/signup/`, payload);
+  return testjanAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return testjanAPI.post(`/rest-auth/login/`, payload);
+  return testjanAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return testjanAPI.get(`/rest-auth/logout/`);
+  return testjanAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return testjanAPI.post(`/rest-auth/logout/`);
+  return testjanAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return testjanAPI.post(`/rest-auth/password/change/`, payload);
+  return testjanAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return testjanAPI.post(`/rest-auth/password/reset/`, payload);
+  return testjanAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return testjanAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return testjanAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return testjanAPI.post(`/rest-auth/registration/`, payload);
+  return testjanAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return testjanAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return testjanAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return testjanAPI.get(`/rest-auth/user/`);
+  return testjanAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return testjanAPI.put(`/rest-auth/user/`, payload);
+  return testjanAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return testjanAPI.patch(`/rest-auth/user/`, payload);
+  return testjanAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +60,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
